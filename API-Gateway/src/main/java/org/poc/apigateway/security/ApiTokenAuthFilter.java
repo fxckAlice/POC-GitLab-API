843e65b1f09cb6ad22c6ed4f06a4149c5c96c90c
Alexander <mykhailovaleksandr3@gmail.com>
2025-07-31 00:40:26 +0300

    api gateway part created

---
+++ API-Gateway/src/main/java/org/poc/apigateway/security/ApiTokenAuthFilter.java
@@ -0,0 +1,59 @@
+package org.poc.apigateway.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ApiTokenAuthFilter extends OncePerRequestFilter {
+
+    private final GitLabTokenConfig gitLabTokenConfig;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public ApiTokenAuthFilter(GitLabTokenConfig gitLabTokenConfig) {
+        this.gitLabTokenConfig = gitLabTokenConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String path = request.getRequestURI();
+        if (path.startsWith("/test/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String presentedToken = authHeader.substring(7);
+
+            if (passwordEncoder.matches(presentedToken, gitLabTokenConfig.getHashedToken())) {
+                Authentication auth = new UsernamePasswordAuthenticationToken(
+                        "api-client", null, List.of(new SimpleGrantedAuthority("ROLE_API_CLIENT")));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid API token");
+                return;
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing API token");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
